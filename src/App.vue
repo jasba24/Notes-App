7552e059b59eed103a0f3455edd59a23bbb7cb91
jasba24 <johanbuitrago342@gmail.com>
2021-09-28 13:09:07 -0500

    add operations

--- src/App.vue
+++ src/App.vue
@@ -1,156 +1,46 @@
 <template>
-  <div :class="[darkMode && 'dark', 'body']">
-    <main :class="[darkMode && 'dark', 'main']">
-      <!-- OPTIONS (DARKMODE - LANGUAGE) -->
-      <section class="options">
-        <Darkmode :darkmode="darkmode" @changeDarkMode="changeDarkMode" />
-        <Language @changeLanguage="changeLanguage" />
-      </section>
-      <!-- END OPTIONS (DARKMODE - LANGUAGE) -->
-
-      <section class="content">
-        <!-- NEW NOTE CONTAINER -->
-        <Content className="new-notes" i18n="description1">
-          <div v-if="flag_add" class="message-success">
-            <AlertMessage i18n="success" />
-          </div>
-          <div v-if="flag_delete" class="message-delete">
-            <AlertMessage i18n="failed" />
-          </div>
-          <CreateNoteForm
-            @newTask="newTask"
-            :task="task"
-            :darkMode="darkMode"
-          />
-        </Content>
-        <!-- END NEW NOTE CONTAINER -->
-
-        <!-- MY NOTES (LIST) -->
-        <Content className="my-notes" i18n="description2">
-          <ul class="notes">
-            <li
-              :class="[task.done && 'task-done', darkMode && 'dark', 'note']"
-              v-for="task in tasks"
-              :key="task.id"
-            >
-              <Note
-                :task="task"
-                :darkMode="darkMode"
-                @deleteTask="deleteTask(task.id)"
-                @doneTask="doneTask(task.id)"
-              />
-            </li>
-          </ul>
-        </Content>
-        <!-- END MY NOTES (LIST) -->
-      </section>
-    </main>
-  </div>
+  <h1>Choose one option</h1>
+  <select @click="getSelectedOption" name="operaciones" id="">
+    <option v-for="(o, i) in options" :key="i" :value="o">{{ o }}</option>
+  </select>
+  <br />
+  <br />
+  <input
+    type="number"
+    v-model="numOne"
+    placeholder="Ingresa el primer numero"
+  />
+  <input
+    type="number"
+    v-model="numTwo"
+    placeholder="Ingresa el segundo numero"
+  />
+  <h1>Result: {{ result }}</h1>
 </template>
 
-<script>
-import Darkmode from "@/components/Darkmode"
-import Language from "@/components/Language"
-import CreateNoteForm from "@/components/CreateNoteForm"
-import AlertMessage from "@/components/AlertMessage"
-import Note from "@/components/Note"
-import Content from "@/components/Content"
+<script setup>
+import { ref, computed } from "vue"
 
-export default {
-  name: "App",
+const numOne = ref(0)
+const numTwo = ref(0)
+const options = ref(["opciones", "suma", "resta", "multiplicacion", "division"])
+let operation = ref(null)
 
-  components: {
-    Darkmode,
-    Language,
-    CreateNoteForm,
-    AlertMessage,
-    Note,
-    Content,
-  },
-
-  data() {
-    return {
-      tasks: [],
-      task: {
-        id: "",
-        title: "",
-        done: false,
-      },
-      flag_add: false,
-      flag_delete: false,
-      darkMode: false,
-    }
-  },
-  methods: {
-    loadData() {
-      if (localStorage.getItem("notes")) {
-        this.tasks = JSON.parse(localStorage.getItem("notes"))
-      } else {
-        localStorage.setItem("notes", JSON.stringify([]))
-      }
-
-      if (localStorage.getItem("darkMode")) {
-        this.darkMode = JSON.parse(localStorage.getItem("darkMode"))
-      } else {
-        localStorage.setItem("darkMode", JSON.stringify([]))
-      }
-    },
-    newTask() {
-      if (this.task.title) {
-        this.task.id = this.uuidv4()
-        this.tasks.push({ ...this.task })
-        setTimeout(() => {
-          this.flag_add = !this.flag_add
-        }, 3000)
-        localStorage.setItem("notes", JSON.stringify(this.tasks))
-        this.task.title = ""
-        this.task.id = ""
-        this.flag_add = !this.flag_add
-      }
-    },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id)
-      setTimeout(() => {
-        this.flag_delete = !this.flag_delete
-      }, 3000)
-      localStorage.setItem(
-        "notes",
-        JSON.stringify(this.tasks, this.darkMode, this.english)
-      )
-      this.flag_delete = !this.flag_delete
-    },
-    doneTask(id) {
-      this.tasks = this.tasks.map((task) => {
-        if (task.id === id) {
-          return {
-            title: task.title,
-            id: task.id,
-            done: !task.done,
-          }
-        }
-        return task
-      })
-      localStorage.setItem("notes", JSON.stringify(this.tasks))
-    },
-    changeDarkMode() {
-      this.darkMode = !this.darkMode
-      localStorage.setItem("darkMode", JSON.stringify(this.darkMode))
-    },
-    uuidv4() {
-      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-        (
-          c ^
-          (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-        ).toString(16)
-      )
-    },
-  },
-  created() {
-    this.loadData()
-  },
+const getSelectedOption = (ev) => {
+  operation.value = ev.target.value
 }
+
+const result = computed(() => {
+  const operations = {
+    suma: numOne.value + numTwo.value,
+    resta: numOne.value - numTwo.value,
+    multiplicacion: numOne.value * numTwo.value,
+    division: numOne.value / numTwo.value,
+  }
+  return operations[operation.value]
+})
 </script>
 
-<style>
-@import url("./index.css");
-</style>
+<script setup></script>
+
+<style></style>
